4b9d6dadcf4c218e68dad93cd3f92ff3a7cdf5a8
Karl Litterfeldt <kalle.litterfeldt@gmail.com>
2014-08-12 22:10:50 +0200

    removed unnecessary binaries

--- android/bin/AGhast-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/gabril/dev/private/AGhast/platforms/android/bin/AGhast-debug-unaligned.apk : \
-/Users/gabril/dev/private/AGhast/platforms/android/bin/AGhast.ap_ \
-/Users/gabril/dev/private/AGhast/platforms/android/bin/classes.dex \
-/Users/gabril/dev/private/AGhast/platforms/android/CordovaLib/bin/classes.jar \
